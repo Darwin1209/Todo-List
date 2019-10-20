c4c7ec9d25162391507f6306690a1cc3fc7e650e
Денис <denis.darwin12@gmail.com>
2019-10-21 01:11:04 +0300

    Добавили обработку событий по нажатию на элементы списка

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,31 +2,55 @@ import React from 'react';
 import './TodoListItem.css'
 
 class TodoListItem extends React.Component {
+    
+    state = {
+        done: false,
+        important: false,
+    };
 
     onLabelClick = () => {
-        console.log(`Done: ${this.props.label}`)
+        if (this.state.done === false) {
+            this.setState( { done: true } );
+        } else {
+            this.setState( { done: false } );
+        }
+    };
+
+    onMarkImportant = () => {
+        if (this.state.important === false) {
+            this.setState( { important: true } );
+        } else {
+            this.setState( { important: false } )
+        }
     }
 
     render() {
 
-        const { label, important = false } = this.props;
+        const { label } = this.props;
+        const { done, important } = this.state;
 
-        const liStyle = {
-            color: important ? "tomato" : "blac",
-            fontWeight: important ? 'bold' : 'normal'
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        } /*else {
+            classNames -= ' done';
+        }*/
+
+        if (important) {
+            classNames += ' important';
         }
          
         return (
-            <div className="todo-list-item">
+            <div className={classNames}>
                 <span 
                     className="todo-list-item-label"
-                    style={liStyle}
                     onClick = { this.onLabelClick }>
                     {label}
                 </span>
     
                 <button type="button"
-                    className="btn btn-outline-success btn-sm float-right">
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick = {this.onMarkImportant}>
                     <i className="fa fa-exclamation" />
                 </button>
     
